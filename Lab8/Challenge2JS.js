18e43cebef332195414b51f0c6bcf1c3f27e4502
Rachel Ruskoff <35467237+RachelRuskoff@users.noreply.github.com>
2019-11-08 12:39:02 -0500

    Lab 8 commit.

--- Lab8/Challenge2JS.js
+++ Lab8/Challenge2JS.js
@@ -1,7 +1,7 @@
 //Sets the initial colours of the three elements on page load
-document.getElementById("blue").style.color="blue";
-document.getElementById("green").style.color="green";
-document.getElementById("red").style.color="red";
+document.getElementById("blue").style.color = "blue";
+document.getElementById("green").style.color = "green";
+document.getElementById("red").style.color = "red";
 // These colours wouldn't show on page load, I'm not sure what I did wrong. I tried changing their Ids to not be colour names but that still didn't work.
 
 //A function to change colours of elements when called
@@ -11,8 +11,8 @@ function mix(){
 	var second = "BB9320";
 	var third = "#AB00FF";
 
-	document.getElementById("blue").style.color=first;
-	document.getElementById("green").style.colour=second;
-	document.getElementById("red").style.color=third;
+	document.getElementById("blue").style.color= first;
+	document.getElementById("green").style.colour= second;
+	document.getElementById("red").style.color= third;
 
 }
